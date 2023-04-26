6ef09396194ba0f768aef9074e814ff0161f28f5
Ciwankurd <s351927@oslomet.no>
2023-04-26 18:51:40 +0200

    update25

--- scripts.js
+++ scripts.js
@@ -90,17 +90,17 @@ async function setCheckedBtn() {
         video: {
             facingMode: "environment",
             //resizeMode: 'none',
-            //width: {ideal: 1280},
-            //height: {ideal: 720},
+            width: {ideal: 1280},
+            height: {ideal: 720},
             //advanced: [{ width: 1920, height: 1280 },{zoom: 1.5}, { aspectRatio: 1.333 }],
             //advanced: [{ width: 1280, height: 720 },{zoom: 1}, { aspectRatio: 1.333 }],
             //focusMode: true,
-            zoom: 1.7,
+            zoom: 1.8,
             //tilt: true,
             //frameRate: 40,
             //pan: true,
-            //scale: true,
-            //aspectRatio: 16 / 9,
+            scale: true,
+            aspectRatio: 16 / 9,
             //deviceId:  devices[2].deviceId
         }
         /*
